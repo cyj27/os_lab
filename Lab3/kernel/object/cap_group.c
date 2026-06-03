e2b8ee5dd7234d8913263d5d4151b33223548985
cyj <cyj061203@sjtu.edu.cn>
2026-06-03 22:21:36 +0800

    lab3

--- Lab3/kernel/object/cap_group.c
+++ Lab3/kernel/object/cap_group.c
@@ -348,15 +348,24 @@ cap_t sys_create_cap_group(unsigned long cap_group_args_p)
         /* cap current cap_group */
         /* LAB 3 TODO BEGIN */
         /* Allocate a new cap_group object */
-
-        /* LAB 3 TODO END */
+        new_cap_group = obj_alloc(TYPE_CAP_GROUP, sizeof(*new_cap_group));
         if (!new_cap_group) {
                 r = -ENOMEM;
                 goto out_fail;
         }
+        /* initialize cap group with user args */
+        r = cap_group_init_user(new_cap_group, BASE_OBJECT_NUM, &args);
+        if (r < 0) {
+                goto out_free_obj_new_grp;
+        }
+        /* LAB 3 TODO END */
         /* LAB 3 TODO BEGIN */
         /* initialize cap group from user*/
-
+        vmspace = obj_alloc(TYPE_VMSPACE, sizeof(*vmspace));
+        if (!vmspace) {
+                r = -ENOMEM;
+                goto out_free_obj_new_grp;
+        }
         /* LAB 3 TODO END */
 
         cap = cap_alloc(current_cap_group, new_cap_group);
@@ -380,7 +389,7 @@ cap_t sys_create_cap_group(unsigned long cap_group_args_p)
 
         /* 2st cap is vmspace */
         /* LAB 3 TODO BEGIN */
-
+        
         /* LAB 3 TODO END */
 
         if (!vmspace) {
@@ -418,30 +427,37 @@ struct cap_group *create_root_cap_group(char *name, size_t name_len)
         cap_t slot_id;
 
         /* LAB 3 TODO BEGIN */
-        UNUSED(vmspace);
-        UNUSED(cap_group);
+        cap_group = obj_alloc(TYPE_CAP_GROUP, sizeof(*cap_group));
+        if (!cap_group) {
+                kwarn("failed alloc cap_group in %s\n", __func__);
+                return NULL;
+        }
 
         /* LAB 3 TODO END */
-        BUG_ON(!cap_group);
 
         /* LAB 3 TODO BEGIN */
         /* initialize cap group with common, use ROOT_CAP_GROUP_BADGE */
-
+        cap_group_init_common(cap_group,
+                              BASE_OBJECT_NUM,
+                              ROOT_CAP_GROUP_BADGE);
         /* LAB 3 TODO END */
         slot_id = cap_alloc(cap_group, cap_group);
 
         BUG_ON(slot_id != CAP_GROUP_OBJ_ID);
 
         /* LAB 3 TODO BEGIN */
-
+        vmspace = obj_alloc(TYPE_VMSPACE, sizeof(*vmspace));
+        if (!vmspace) {
+                kwarn("failed alloc vmspace in %s\n", __func__);
+                return NULL;
+        }
         /* LAB 3 TODO END */
-        BUG_ON(!vmspace);
 
         /* fixed PCID 1 for root process, PCID 0 is not used. */
         vmspace_init(vmspace, ROOT_PROCESS_PCID);
 
         /* LAB 3 TODO BEGIN */
-
+        slot_id = cap_alloc(cap_group, vmspace);
         /* LAB 3 TODO END */
 
         BUG_ON(slot_id != VMSPACE_OBJ_ID);
--- Lab3/kernel/object/thread.c
+++ Lab3/kernel/object/thread.c
@@ -216,8 +216,17 @@ void create_root_thread(void)
         BUG_ON(thread == NULL);
 
         for (int i = 0; i < meta.phnum; i++) {
-                unsigned int flags;
+                unsigned int type, flags;
                 unsigned long offset, vaddr, filesz, memsz;
+
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_TYPE_OFF),
+                       sizeof(data));
+                type = (unsigned int)le32_to_cpu(*(u32 *)data);
+                if (type != 1)
+                        continue;
 
                 memcpy(data,
                        (void *)((unsigned long)&binary_procmgr_bin_start
@@ -228,30 +237,60 @@ void create_root_thread(void)
 
                 /* LAB 3 TODO BEGIN */
                 /* Get offset, vaddr, filesz, memsz from image*/
-                UNUSED(flags);
-                UNUSED(filesz);
-                UNUSED(offset);
-                UNUSED(memsz);
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_OFFSET_OFF),
+                       sizeof(data));
+                offset = (unsigned long)le64_to_cpu(*(u64 *)data);
 
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_VADDR_OFF),
+                       sizeof(data));
+                vaddr = (unsigned long)le64_to_cpu(*(u64 *)data);
+
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_FILESZ_OFF),
+                       sizeof(data));
+                filesz = (unsigned long)le64_to_cpu(*(u64 *)data);
+
+                memcpy(data,
+                       (void *)((unsigned long)&binary_procmgr_bin_start
+                                + ROOT_PHDR_OFF + i * ROOT_PHENT_SIZE
+                                + PHDR_MEMSZ_OFF),
+                       sizeof(data));
+                memsz = (unsigned long)le64_to_cpu(*(u64 *)data);
                 /* LAB 3 TODO END */
 
                 struct pmobject *segment_pmo = NULL;
                 /* LAB 3 TODO BEGIN */
-                UNUSED(segment_pmo);
-
+                ret = create_pmo(memsz, PMO_DATA, root_cap_group, 0, &segment_pmo,
+                                 PMO_ALL_RIGHTS);
                 /* LAB 3 TODO END */
 
                 BUG_ON(ret < 0);
 
                 /* LAB 3 TODO BEGIN */
                 /* Copy elf file contents into memory*/
-
+                memset((void *)phys_to_virt(segment_pmo->start), 0, segment_pmo->size);
+                memcpy((void *)phys_to_virt(segment_pmo->start),
+                       (void *)((unsigned long)&binary_procmgr_bin_start + offset),
+                       filesz);
                 /* LAB 3 TODO END */
 
                 unsigned vmr_flags = 0;
                 /* LAB 3 TODO BEGIN */
                 /* Set flags*/
-
+                if (flags & PHDR_FLAGS_R)
+                        vmr_flags |= VMR_READ;
+                if (flags & PHDR_FLAGS_W)
+                        vmr_flags |= VMR_WRITE;
+                if (flags & PHDR_FLAGS_X)
+                        vmr_flags |= VMR_EXEC;
                 /* LAB 3 TODO END */
 
                 ret = vmspace_map_range(init_vmspace,
